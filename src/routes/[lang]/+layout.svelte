99b65aa94676398a1a2931c0ebe28c55468d60cd
Christian Helbling <helbling@search.ch>
2023-12-29 16:36:20 +0100

    work on layout, translations, first content

--- src/routes/[lang]/+layout.svelte
+++ src/routes/[lang]/+layout.svelte
@@ -1,49 +1,36 @@
 <script>
 	export const title = '37th Swiss Juggling Convention Winterthur';
 	export let data;
-	let lang, logo, page;
+	let lang, page;
 	$: lang = data.lang;
 	$: page = data.route.id.replace(/\/\[lang\]/, '');
-	logo = 1;
 </script>
 
 <style>
-	:global(html),:global(body) { height:100%; padding:0; margin:0; font-family: 'Open Sans', Helvetica, Arial, sans-serif; background:#f8f8f8; }
+	:global(html),:global(body) { height:100%; padding:0; margin:0; font-family: 'Open Sans', Helvetica, Arial, sans-serif; background:#f8f8f8; font-size:18px }
 	:global(html) { overflow-y:scroll; }
 	#container { margin:0 auto; max-width:70em; background:#fff; padding:0.5em }
-	nav        { float:left; width:14em}
-	#logo      { width:14em }
-	#main      { margin-left:14em; padding-left:1em; min-height:35em }
-	h1 { margin:auto; text-align:center; min-height:3.5em; background-color:#efb043 }
-	.right { float:right }
-	.nav_begin { margin-bottom:0 }
-	.nav_end { clear:both }
-	nav a        { display:block; text-decoration:none; color:#444; padding:0.5em }
-	nav .logo a, nav .language a  { display:inline }
-	nav a:hover, nav a.active  { background-color:#eee }
+	nav { background-color:#f0b045; padding:0.5em }
+	nav a { display:block; text-decoration:none; color:#444; padding:0.5em }
+	nav .language a  { display:inline }
+	nav a:hover, nav a.active  { background-color:#fff }
 	nav a.active { font-weight:bold }
-	nav .logo { margin-top:-1em; margin-bottom:2em }
 
-	table { margin-top:1em }
-	td    { vertical-align:top }
+	.logo { text-align: center }
+	.logo img { width: 100%; max-width:14em }
 
-	#mapcontainer { width:100%; height:800px;border:1px solid #888 }
-	table.ok th { text-align:left; padding-right:1em}
-	.gelaende table th { text-align:left; padding-right:1em}
+	h1 { display: none }
 
-	table.workshops th { text-align:left; padding-right:1em}
-	table.workshops td { padding-right:1em}
-	table.workshops td.time { white-space:nowrap }
+	@media (max-width:34em) {
+		nav { margin-top:2em; padding:1em }
+		nav a, nav p { display:inline }
+	}
 
-	#content_index img { width:24em }
-
-	#content_partner li, #content_goenner li { margin-bottom:1em; clear:right }
-	#content_partner li div.logo, #content_goenner li div.logo { float:right; margin-bottom:2em }
-
-	#container.fotos table td { padding:0.5em; text-align:center; vertical-align:middle }
-
-	@media (max-width:600px) {
-		img.right { display:none }
+	@media (min-width:34em) {
+		.logo, nav { width:14em }
+		h1 { display: block; margin:auto; text-align:center; min-height:3em; background-color:#f0b045 }
+		nav { position:absolute; top:14em; margin-left:2em; width:9em }
+		#main { position:relative; top:-14em; margin-left:14em; padding-left:1em; min-height:35em }
 	}
 </style>
 
@@ -57,25 +44,22 @@
 </svelte:head>
 
 <div id="container" class="index">
-	<nav>
-		<a href="/{lang}"><img id="logo" src="/img/logo{logo}_transparent.png" alt="Logo {title}"></a>
-		<p class=logo>
-			Logo:
-			<a class:active={logo==1} on:click={() => {logo=1}}>1</a>
-			<a class:active={logo==2} on:click={() => {logo=2}}>2</a>
-			<a class:active={logo==3} on:click={() => {logo=3}}>3</a>
-		</p>
-
-		<p class=language>
-			<a class:active={lang=='de'} href="{data.route.id.replace(/\[lang\]/, 'de')}">DE</a>
-			<a class:active={lang=='en'} href="{data.route.id.replace(/\[lang\]/, 'en')}">EN</a>
-		</p>
-
-		<a class:active={page==''}          href="/{lang}/">Home</a>
-		<a class:active={page=='/programm'} href="/{lang}/programm">Programm</a>
-	</nav>
+	<div class=logo>
+		<a href="/{lang}" class=logo><img src="/img/logo504.png" alt="Logo {title}"></a>
+	</div>
 	<div id="main">
 		<h1>{title}</h1>
 		<slot></slot>
 	</div>
+	<nav>
+		<div class=bar>
+			<p class=language>
+				<a class:active={lang=='de'} href="{data.route.id.replace(/\[lang\]/, 'de')}">DE</a>
+				<a class:active={lang=='en'} href="{data.route.id.replace(/\[lang\]/, 'en')}">EN</a>
+			</p>
+
+			<a class:active={page==''}          href="/{lang}/">Home</a>
+			<a class:active={page=='/programm'} href="/{lang}/programm">Programm</a>
+		</div>
+	</nav>
 </div>
--- src/routes/[lang]/+page.svelte
+++ src/routes/[lang]/+page.svelte
@@ -1,5 +1,51 @@
 <script>
 	export let data;
+	let t, lang;
+	$: t = data.t;
+	$: lang = data.lang;
 </script>
 
-<h1>30.8. - 1.9.2024 in Winterthur Neuhegi</h1>
+<style>
+	div { text-align: center }
+	ul { list-style-type:none; padding:0; margin:0 }
+	.bold { font-weight:bold }
+</style>
+
+<div>
+
+<p>
+Eulachpark / Halle 710 / Neuhegi
+</p>
+
+{#if lang == 'de'}
+<h2>Freitag 30.8. - Sonntag 1.9.2024</h2>
+
+<ul>
+	<li>Jonglieren &amp; Artistik rund um die Uhr</li>
+	<li class=bold>Workshops für alle Niveaus</li>
+	<li>Jonglierspiele</li>
+	<li>&nbsp;</li>
+</ul>
+<ul>
+	<li>Juggling Battles</li>
+	<li class=bold>Galashow</li>
+	<li>Fire Space &amp; Light Night</li>
+</ul>
+
+{:else}
+<h2>Friday 30.8. until Sunday 1.9.2024</h2>
+
+<ul>
+	<li>Juggling &amp; Artistry around the clock</li>
+	<li class=bold>Workshops für all skill-levels</li>
+	<li>Juggling Games</li>
+	<li>&nbsp;</li>
+</ul>
+<ul>
+	<li>Juggling Battles</li>
+	<li class=bold>Galashow</li>
+	<li>Fire Space &amp; Light Night</li>
+</ul>
+
+{/if}
+</div>
