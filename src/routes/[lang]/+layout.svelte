7f7e60d883ecf0c93e04fdb25d64d15c4c999bd3
Christian Helbling <helbling@search.ch>
2024-01-01 22:08:59 +0100

    add side-scroll buttons for mobile navigation

--- src/routes/[lang]/+layout.svelte
+++ src/routes/[lang]/+layout.svelte
@@ -1,9 +1,13 @@
 <script>
+	import { onMount } from 'svelte';
+
 	export const title = '37th Swiss Juggling Convention Winterthur';
 	export let data;
-	let lang, page;
+	let lang, page, nav;
 	$: lang = data.lang;
 	$: page = data.route.id.replace(/\/\[lang\]\/?/, '');
+	let arrow_left_visible = false;
+	let arrow_right_visible = false;
 
 	const pages = [
 		{ page: '',          de:'Home' },
@@ -19,6 +23,14 @@
 		// { page: 'medien',       de:'Medien' },
 		// { page: 'kontakt',      de:'Kontakt' },
 	];
+
+	function navscroll(e) {
+		arrow_left_visible = nav.scrollLeft > 0;
+		arrow_right_visible = nav.scrollLeft < nav.scrollWidth - nav.clientWidth;
+	}
+	onMount(() => {
+		navscroll();
+	});
 </script>
 
 <style>
@@ -28,15 +40,24 @@
 	nav { background-color:#f0b045; padding:0.5em }
 	nav a { display:block; text-decoration:none; color:#444; padding:0.5em }
 	nav .language a  { display:inline }
-	nav a:hover, nav a.active  { background-color:#fff }
-	nav a.active { font-weight:bold }
+	nav a.active { background-color:rgba(255,255,255,0.6); font-weight:bold }
+	nav a:hover  { background-color:rgba(255,255,255,0.9) }
 
 	.logo { text-align: center }
 	.logo img { width: 100%; max-width:14em }
 
 	h1 { display: none }
+	.arrow { display:none }
 
 	@media (max-width:42em) {
+		.arrow.visible { display:block;  }
+		.arrow { position:absolute; background:#f0b045; cursor:pointer }
+		.arrow.left  { left: 0.5em }
+		.arrow.right { right:0.5em }
+
+		.arrow.left.fade  { left:  2.2em; background:linear-gradient(to right, rgba(240,176,69,1), rgba(240,176,69,0)) }
+		.arrow.right.fade { right: 2.2em; background:linear-gradient(to left, rgba(240,176,69,1), rgba(240,176,69,0)) }
+
 		nav { overflow-x:auto; display:flex; padding:0.5em; white-space: nowrap }
 		nav p { display:flex; padding:0; margin:0; margin-right:1em }
 
@@ -71,7 +92,30 @@
 		<a href="/{lang}" class=logo><img src="/img/logo504.png" alt="Logo {title}"></a>
 	</div>
 
-	<nav>
+	<nav bind:this={nav} on:scroll={navscroll}>
+		<a
+			class="arrow left"
+			class:visible={arrow_left_visible}
+			on:click|preventDefault={() => { nav.scrollLeft -= nav.clientWidth / 2}}
+			href={null}
+		>&nbsp;&lt;</a>
+		<a
+			class="arrow left fade"
+			class:visible={arrow_left_visible}
+			href={null}
+		>&nbsp;</a>
+		<a
+			class="arrow right fade"
+			class:visible={arrow_right_visible}
+			href={null}
+		>&nbsp;</a>
+		<a
+			class="arrow right"
+			class:visible={arrow_right_visible}
+			on:click|preventDefault={() => { nav.scrollLeft += nav.clientWidth / 2}}
+			href={null}
+		>&gt;&nbsp;</a>
+
 		<p class=language>
 			<a class:active={lang=='de'} href="{data.route.id.replace(/\[lang\]/, 'de')}">DE</a>
 			<a class:active={lang=='en'} href="{data.route.id.replace(/\[lang\]/, 'en')}">EN</a>
