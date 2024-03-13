b9fb1b82b33024781f3ae8e6bad6eed362810a01
Christian Helbling <helbling@search.ch>
2024-03-13 08:34:53 +0100

    whitespace fix

--- src/routes/[lang]/kontakt/+page.svelte
+++ src/routes/[lang]/kontakt/+page.svelte
@@ -12,7 +12,7 @@
 <style>
 	iframe { border: none; width:100%; max-width:30em; height:52em }
 </style>
-<h2>Verein “Swiss Juggling Convention 2024 Winterthur ”, Winterthur</h2>
+<h2>Verein “Swiss Juggling Convention 2024 Winterthur”, Winterthur</h2>
 
 <h3>{t_contact_form}</h3>
 
